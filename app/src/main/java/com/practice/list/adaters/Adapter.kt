476471bc1e9961a1d3980694a8f4ac03039505cd
jykim7760 <jykim7760@naver.com>
2020-04-09 22:17:31 +0900

    리스트작업의 뼈대

---
+++ app/src/main/java/com/practice/list/adaters/Adapter.kt
@@ -0,0 +1,28 @@
+package com.practice.list.adaters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import com.practice.list.R
+import com.practice.list.datas.Shoes
+import kotlin.coroutines.coroutineContext
+
+class Adapter(context:Context, resId:Int, List:ArrayList<Shoes>) : ArrayAdapter<Shoes> (context, resId, List)
+
+
+val mContext = Context
+val mList = List
+val inf = LayoutInflater.from(mContext)
+
+
+getview =
+
+    var temRow = converView
+    if(temRow == null){
+    temRow = inf.inflate(R.layout.room_list_item)
+}
+
+val row = tempRow!!
+return row
+}
+}
