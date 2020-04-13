b96144d269fc4a4d9cf9bd9792017d53d74401a5
jykim7760 <jykim7760@naver.com>
2020-04-13 20:50:03 +0900

    실데이터를 리스트에 반영

--- app/src/main/java/com/practice/list/MainActivity.kt
+++ app/src/main/java/com/practice/list/MainActivity.kt
@@ -2,11 +2,14 @@ package com.practice.list
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.practice.list.adaters.Adapter
 import com.practice.list.datas.Shoes
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val roomList = ArrayList<Shoes>()
+    var mRoomAdapter:Adapter?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +23,9 @@ class MainActivity : AppCompatActivity() {
         roomList.add(Shoes(8000, "서울시 은평구", -1, "사라있네입니다."))
         roomList.add(Shoes(8000, "서울시 은평구", 12, "연립주택입니다."))
 
+
+        mRoomAdapter = Adapter(this, R.layout.room_list_item, roomList)
+        roomListView.adapter = mRoomAdapter
 
     }
 }
--- app/src/main/java/com/practice/list/adaters/Adapter.kt
+++ app/src/main/java/com/practice/list/adaters/Adapter.kt
@@ -20,8 +20,44 @@ val inf = LayoutInflater.from(mContext)
 }
 
 val row = tempRow!!
-return row
 
-//
+//row 데이터 가공 _각 줄에 맞게
+
+//  XML에 잇는 뷰들을 변수로 담기 findViewById
+val pricetxt = row.findViewById<TextView>(R.id.priceTxt)
+val pricetxt = row.findViewById<TextView>(R.id.floorTxt)
+val pricetxt = row.findViewById<TextView>(R.id.subTxt)
+
+// 근거 데이터 변수 추출 -> 각위치에 맞는 데이터를 mList 에서 가져오기
+val data = mList.get(position)
+
+// 가격이 1만이상이면 ?억, 그이하면 그냥 "?"
+if (data.price >= 10000){
+    pricetxt.text = "${data.price / 10000}억 $(String.format("%,d",data.price % 10000))"
+}
+else {
+    pricetxt.text = "String.format("%,d",data.price)
+
+}
+//    주소와 층수 " 주소, 층수(상황마다다른값)
+var floorStr = ""
+
+if (data.floor == 0){
+    floorStr = "반지하"
+}
+else if (data.floor >0){
+    floorStr = "${data.floor}층"
+}
+else{
+    floorStr = "지하${-data.floor}층"
+
+    AddressAndFloorTxt.text = "${data.address}, {data.floor}"
+
+//    설명은 있는 그대로
+    subtxt.text = data.subtxt
+
+
+    return row
+}}
 
 
